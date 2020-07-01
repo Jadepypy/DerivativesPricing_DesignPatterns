6844e591452e06b9d3c6901e06e27d3484f2ae7c
Jadepypy <questiongirl829@gmail.com>
2020-07-01 10:19:40 +0800

    update project

---
+++ Exotic_engine/Antithetic.cpp
@@ -0,0 +1,10 @@
+#include "Antithetic.h"
+
+Antithetic::Antithetic()
+{
+}
+
+Antithetic::~Antithetic()
+{
+}
+
---
+++ Exotic_engine/Antithetic.h
@@ -0,0 +1,12 @@
+#ifndef ANTITHETIC_H
+#define ANTITHETIC_H
+
+class Antithetic
+{
+public:
+    Antithetic();
+    ~Antithetic();
+
+};
+
+#endif // ANTITHETIC_H
